230b2c0857210f05b7e8a98dbdc050bd76e0e376
sethledford <sethledford@URB218-14.seasad.wustl.edu>
2018-03-19 17:23:25 -0500

    Studio 8 attempted.

--- src/studio8/CountSteps.java
+++ src/studio8/CountSteps.java
@@ -1,7 +1,20 @@
 package studio8;
 
+public class CountSteps {
+	
+	
+	public static int peaks(int x, int y, int z) {
+	    
+			return (x, y, z);
+	    }
+	}
+	
+
+
+
 // TODO: Develop an algorithm to count steps in accelerometer data
 //    Major steeps:
 //       1. Create a class and main method.
 //       2. Using a Scanner and File object, read data from your .csv file.
 //       3. Develop and test algorithms to count the "peaks" in the data.
+
---
+++ steps/steps.ino
@@ -0,0 +1,25 @@
+#include <Wire.h> // Must include Wire library for I2C
+#include <SparkFun_MMA8452Q.h> // Includes the SFE_MMA8452Q library
+MMA8452Q accel; // Default MMA8452Q object create. (Address = 0x1D)
+void setup(){
+  Serial.begin(9600);
+  accel.init();
+  }
+
+void loop(){
+
+  accel.read();
+  Serial.println();
+  Serial.print(accel.cx, 3);
+  Serial.print(",");
+  Serial.print(accel.cy, 3);
+  Serial.print(",");
+  Serial.print(accel.cz, 3);
+  Serial.println();
+//  Serial.print(accel.x, 3);
+//  Serial.print(",");
+//  Serial.print(accel.y, 3);
+//  Serial.print(",");
+//  Serial.print(accel.z, 3);
+  
+  }
